8fe4ad81a38d196de53e2ff81be1a8bc52b2fca0
zanattaR <67986361+zanattaR@users.noreply.github.com>
2023-04-24 09:37:09 -0300

    Alteração de token na variável de ambiente

--- mvp_reviews_gpt.py
+++ mvp_reviews_gpt.py
@@ -125,12 +125,12 @@ if reviewSheet is not None:
 			return data
 
 		results = asyncio.run(getChatgptResponses(prompts))
-		st.write(results)
+
 		replies = []
 		for i in results:
 			reply = i['choices'][0]['message']['content']
 			replies.append(reply)
-		st.write(replies)
+
 
 		# Divide a lista em pedaços de três itens e atribui a cada coluna
 		dfPrompt['Resposta_1'] = replies[::3]
